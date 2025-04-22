c9bb23cd29bd70af638dee947082c31d0bf395ae
Daksh Shah <daksh.ks@somaiya.edu>
2025-04-22 23:12:50 +0530

    Update google-config.php

--- auth/google-config.php
+++ auth/google-config.php
@@ -2,8 +2,8 @@
 require_once 'vendor/autoload.php';
 
 $client = new Google_Client();
-$client->setClientId('281943650357-3d6sjk0m9t0coq4t6njje40l14ttv2aa.apps.googleusercontent.com');
-$client->setClientSecret('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');
+$client->setClientId('');
+$client->setClientSecret('');
 $client->setRedirectUri('http://localhost/recipe/auth/google-callback.php');
 $client->addScope('email');
 $client->addScope('profile');
