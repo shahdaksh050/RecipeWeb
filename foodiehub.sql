93fda197a79fe1abd888ab28db3c71e243bc3756
Daksh Shah <daksh.ks@somaiya.edu>
2025-05-27 18:31:01 +0530

    Update foodiehub.sql

--- foodiehub.sql
+++ foodiehub.sql
@@ -170,6 +170,7 @@ CREATE TABLE `users` (
   `email` varchar(150) NOT NULL,
   `password` varchar(255) NOT NULL,
   `address` varchar(255) DEFAULT NULL,
+  `profile_photo` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
